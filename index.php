5ae5888c86a4ed0b0c07a6637492234770d390cf
root <root@localhost.localdomain>
2023-02-17 17:15:22 +0800

    up

--- index.php
+++ index.php
@@ -10,3 +10,21 @@
 require "vendor/autoload.php";
 $Test = new Linpeibing\LpbTool\Test();
 $Test->test();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+// 创建链接
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'lpb_test',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8mb4',
+    'port' => 3306,
+    'collation' => 'utf8mb4_general_ci',
+    'prefix' => '',
+]);
+// 设置全局静态可访问DB
+$capsule->setAsGlobal();
+// 启动Eloquent （如果只使用查询构造器，这个可以注释）
+$capsule->bootEloquent();
