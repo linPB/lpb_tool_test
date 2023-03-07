85d8763dd69d53fe84f047288d018412ecd55297
linpeibing <1938545561@qq.com>
2023-03-07 11:46:51 +0800

    up

--- index.php
+++ index.php
@@ -9,13 +9,75 @@
 
 require "vendor/autoload.php";
 $smp_lists = require "data_list/smp.php";
+$log_lists = require "data_list/log.php";
 
-use Linpeibing\LpbTool\DB;
+# php index.php -p 1
+$do_args = getopt('p:');
 
-// calculateAttendanceForZhiJie("original.xlsx");
+var_dump($do_args);
 
-parseExportCsv2($smp_lists['smp_item_score']['时装']['column'], $smp_lists['smp_item_score']['时装']['sql'], "smp_item_score");
+if (!isset($do_args["p"])) {
+    echo "参数缺失".PHP_EOL;
+    die();
+}
 
+switch ($do_args["p"]){
+    case "export_calc" :
+        calculateAttendanceForZhiJie("original.xlsx");
+        break;
+    case "export_smp":
+        /**
+         *@ 标准输入
+         *@ php://stdin & STDIN
+         *@ STDIN是一个文件句柄，等同于fopen("php://stdin", 'r')
+         */
+        $fh = fopen('php://stdin', 'r');
+        echo "请输入smp表名：";
+        $table = fread($fh, 1000);
+        echo "你输入的是：$table";
+        fclose($fh);
+        echo "请输入导出类型：";
+        $type = fread(STDIN, 1000);
+        echo "你输入的是：$type";
+
+        if (!isset($smp_lists[$table])) {
+            echo "$table 表未配置，操作拒绝".PHP_EOL;
+            die();
+        }
+        if (!isset($smp_lists[$table][$type])) {
+            echo "$table 表对应的 $type 未配置，操作拒绝".PHP_EOL;
+            die();
+        }
+
+        try {
+            parseExportCsv2($smp_lists[$table][$type]['column'], $smp_lists[$table][$type]['sql'], $table);
+        } catch (Exception $e) {
+            var_dump($e);
+        }
+
+        break;
+    case "export_log":
+        $fh = fopen('php://stdin', 'r');
+        echo "请输入log表名：";
+        $table = fread($fh, 1000);
+        echo "你输入的是：$table";
+        fclose($fh);
+        echo "请输入导出类型：";
+        $type = fread(STDIN, 1000);
+        echo "你输入的是：$type";
+        break;
+    case "test":
+        try {
+            $rows = (new Linpeibing\LpbTool\DB)->get()::table("test")->get();
+            var_dump($rows);
+        } catch (Exception $e) {
+            var_dump($e);
+        }
+        break;
+    default:
+        echo "操作不允许".PHP_EOL;
+        die();
+}
 
 
 
